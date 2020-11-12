58e49a90cd0311851d9c4fb5cacedd5b289e5c29
Nick Carducci <nmcarducci@gmail.com>
2020-11-12 16:13:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import firebase from "./init-firebase.js";
 import "./styles.css";
 
 class App extends React.Component {
-  state = { counted: 0 };
+  state = { signatures: [] };
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("signatures")
+      .onSnapshot((querySnapshot) => {
+        let signatures = [];
+        let p = 0;
+        querySnapshot.docs.forEach((doc) => {
+          p++;
+          if (doc.exists) {
+            var foo = doc.data();
+            foo.id = doc.id;
+            signatures.push(foo);
+          }
+          if (querySnapshot.docs.length === p) {
+            this.setState({ signatures });
+          }
+        });
+      });
+  };
   render() {
     return (
       <div className="App">
@@ -28,54 +48,73 @@ class App extends React.Component {
           </div>
         </h1>
         <h2>Are you a New Jersey voter?</h2>
-        <h2>Submit your signature! {this.state.counted}/800</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            firebase.firestore().collection("signatures").add({
-              first: this.state.first,
-              middle: this.state.middle,
-              last: this.state.last,
-              address: this.state.address,
-              city: this.state.city,
-              zip: this.state.zip
-            });
-          }}
-        >
-          <input
-            onChange={(e) => this.setState({ first: e.target.value })}
-            placeholder="first name"
-          />
-          <input
-            onChange={(e) => this.setState({ middle: e.target.value })}
-            placeholder="middle name"
-          />
-          <input
-            onChange={(e) => this.setState({ last: e.target.value })}
-            placeholder="last name"
-          />
-          <br />
-          <input
-            onChange={(e) => this.setState({ address: e.target.value })}
-            placeholder="address"
-          />
-          <input
-            onChange={(e) => this.setState({ city: e.target.value })}
-            placeholder="city"
-          />
-          <input
-            onChange={(e) => this.setState({ zip: e.target.value })}
-            placeholder="zip"
-          />
-          <div style={{ fontSize: "12px" }}>
-            This provisional signature to get on US Sentate ballot in 2022 will
-            be contestable if <br />
-            voter identity is ambiguous{" "}
-            <a href="https://voter.svrs.nj.gov/registration-check">
-              https://voter.svrs.nj.gov/registration-check
-            </a>
-          </div>
-        </form>
+        <h2>Submit your signature! {this.state.signatures.length}/800</h2>
+        {this.state.finished ? (
+          <h2>Thank you!</h2>
+        ) : (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (
+                this.state.first &&
+                this.state.middle &&
+                this.state.last &&
+                this.state.address &&
+                this.state.city &&
+                this.state.zip
+              )
+                firebase
+                  .firestore()
+                  .collection("signatures")
+                  .add({
+                    first: this.state.first,
+                    middle: this.state.middle,
+                    last: this.state.last,
+                    address: this.state.address,
+                    city: this.state.city,
+                    zip: this.state.zip
+                  })
+                  .then(() => {
+                    this.setState({ finished: true });
+                  })
+                  .catch((err) => console.log(err.message));
+            }}
+          >
+            <input
+              onChange={(e) => this.setState({ first: e.target.value })}
+              placeholder="first name"
+            />
+            <input
+              onChange={(e) => this.setState({ middle: e.target.value })}
+              placeholder="middle name"
+            />
+            <input
+              onChange={(e) => this.setState({ last: e.target.value })}
+              placeholder="last name"
+            />
+            <br />
+            <input
+              onChange={(e) => this.setState({ address: e.target.value })}
+              placeholder="address"
+            />
+            <input
+              onChange={(e) => this.setState({ city: e.target.value })}
+              placeholder="city"
+            />
+            <input
+              onChange={(e) => this.setState({ zip: e.target.value })}
+              placeholder="zip"
+            />
+            <div style={{ fontSize: "12px" }}>
+              This provisional signature to get on US Sentate ballot in 2022
+              will be contestable if <br />
+              voter identity is ambiguous{" "}
+              <a href="https://voter.svrs.nj.gov/registration-check">
+                https://voter.svrs.nj.gov/registration-check
+              </a>
+            </div>
+          </form>
+        )}
         <h2>"Invoices are theft!"</h2>
         <h2>- Nick Carducci, 2020</h2>
         <div>I will support legislation that retains equity</div>
